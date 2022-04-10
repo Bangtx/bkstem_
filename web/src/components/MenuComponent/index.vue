b068b112f953341218d0cd5ad27cfb05f905e944
namhoang2210 <namle.22102000@gmail.com>
2022-04-10 23:25:31 +0700

    fix css side bar admin

--- web/src/components/MenuComponent/index.vue
+++ web/src/components/MenuComponent/index.vue
@@ -52,31 +52,33 @@
                 span.hidden(class="md:ml-4 md:block") Giao bài tập
                 span(class="md:hidden ") Bài tập
 
-          .flex.justify-between(v-if="isAdmin")
-            .relative(class='lg:px-6 lg:py-3')
-              a.items-center.w-full.font-semibold.transition-colors.duration-150(class='lg:inline-flex hover:text-gray-800' @click="$emit('on-click', 'class_management')")
-                .flex.justify-center
-                  svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
-                    path(stroke-linecap='round' stroke-linejoin='round' d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253')
-                span.hidden(class='lg:ml-4 lg:block') Quản lí môn học
-                span(class='lg:hidden lg:ml-4 lg:block') QL.Môn học
-            .relative(class='lg:px-6 lg:py-3')
-              a.items-center.w-full.font-semibold.transition-colors.duration-150(class='lg:inline-flex hover:text-gray-800' @click="$emit('on-click', 'teacher_management')")
-                .flex.justify-center
-                  svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
-                    path(stroke-linecap='round' stroke-linejoin='round' d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z')
-                span.hidden(class='lg:ml-4 lg:block') Quản lí giáo viên
-                span(class='lg:hidden lg:ml-4 lg:block') QL.Giáo viên
-            .relative(class='lg:px-6 lg:py-3')
-              a.items-center.w-full.font-semibold.transition-colors.duration-150(class='lg:inline-flex hover:text-gray-800' @click="$emit('on-click', 'student_management')")
-                .flex.justify-center
-                  svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
-                    path(d='M12 14l9-5-9-5-9 5 9 5z')
-                    path(d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z')
-                    path(stroke-linecap='round' stroke-linejoin='round' d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222')
-                span.hidden(class='lg:ml-4 lg:block') Quản lí học viên
-                span(class='lg:hidden lg:ml-4 lg:block') QL.Học viên
-
+          div(v-if="isAdmin")
+            .bg-white.pb-2.fixed.bottom-0.z-10.w-full.content-center(class='lg:bg-gray-50 lg:relative lg:h-screen lg:w-60')
+              .text-sm.content-center.text-left.justify-between(class='lg:mt-16 lg:w-60 lg:fixed lg:left-0 lg:top-0 lg:content-start')
+                .list-reset.flex.justify-between.flex-row.pt-3.px-2.text-center.text-gray-600(class='lg:flex-col lg:py-3 lg:px-2 lg:text-left')
+                  .relative(class='lg:px-6 lg:py-3')
+                    a.items-center.w-full.font-semibold.transition-colors.duration-150(class='lg:inline-flex hover:text-gray-800' @click="$emit('on-click', 'class_management')")
+                      .flex.justify-center
+                        svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
+                          path(stroke-linecap='round' stroke-linejoin='round' d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253')
+                      span.hidden(class='lg:ml-4 lg:block') Quản lí môn học
+                      span(class='lg:hidden lg:ml-4 lg:block') QL.Môn học
+                  .relative(class='lg:px-6 lg:py-3')
+                    a.items-center.w-full.font-semibold.transition-colors.duration-150(class='lg:inline-flex hover:text-gray-800' @click="$emit('on-click', 'teacher_management')")
+                      .flex.justify-center
+                        svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
+                          path(stroke-linecap='round' stroke-linejoin='round' d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z')
+                      span.hidden(class='lg:ml-4 lg:block') Quản lí giáo viên
+                      span(class='lg:hidden lg:ml-4 lg:block') QL.Giáo viên
+                  .relative(class='lg:px-6 lg:py-3')
+                    a.items-center.w-full.font-semibold.transition-colors.duration-150(class='lg:inline-flex hover:text-gray-800'  @click="$emit('on-click', 'student_management')")
+                      .flex.justify-center
+                        svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
+                          path(d='M12 14l9-5-9-5-9 5 9 5z')
+                          path(d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z')
+                          path(stroke-linecap='round' stroke-linejoin='round' d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222')
+                      span.hidden(class='lg:ml-4 lg:block') Quản lí học viên
+                      span(class='lg:hidden lg:ml-4 lg:block') QL.Học viên
 </template>
 
 <script lang="ts">
