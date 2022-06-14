d067f201d72103ce0fa1e8a926305c9d4788b348
Bangtx <bangtran.hha@gmail.com>
2022-06-14 21:30:15 +0700

    http://lms.bksteam.edu.vn/

--- api/main.py
+++ api/main.py
@@ -20,7 +20,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[WEB_URL_PRO, WEB_URL],
+    # allow_origins=[WEB_URL_PRO, WEB_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
